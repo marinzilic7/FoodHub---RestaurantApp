b56add978db9db66b24ec52e5a7d3ae4c7caeb8f
NiinaLjubic <nikola.ljubic@fpmoz.sum.ba>
2024-01-15 23:56:02 +0100

    dodana funkcionalnost mjenanja avatara na osnovu spola (home activity)

--- app/src/main/java/com/example/foodhub/HomeActivity.java
+++ app/src/main/java/com/example/foodhub/HomeActivity.java
@@ -1,12 +1,21 @@
 package com.example.foodhub;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+
+
 
 public class HomeActivity extends AppCompatActivity {
     @Override
@@ -14,6 +23,48 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        ImageView avatar = findViewById(R.id.avatarWelcome);
 
+
+        TextView welcomeUser = findViewById(R.id.welcomeUser);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference base = FirebaseDatabase.getInstance().getReference("users");
+        if(user != null){
+            String userId = user.getUid();
+
+            base.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        String gender = snapshot.child("gender").getValue(String.class);
+                        String firstName = snapshot.child("firstName").getValue(String.class);
+                        String lastName = snapshot.child("lastName").getValue(String.class);
+                        welcomeUser.setText("Welcome " + firstName + " " + lastName);
+                        if ("male".equalsIgnoreCase(gender)) {
+                            avatar.setImageResource(R.mipmap.avatar);
+                        } else {
+                            avatar.setImageResource(R.mipmap.girl_avatar);
+                        }
+
+                    }else{
+                        startLogoutActivity();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    startLogoutActivity();
+                }
+            });
+        }else{
+            startLogoutActivity();
+        }
+
+    }
+
+    private void startLogoutActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
