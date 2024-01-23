7bc3149d556125eb1a3d35fbb14a3d154e2e7ca2
marinzilic7 <marinzilic507@gmail.com>
2024-01-23 20:58:21 +0100

    dodan dependencies Glide kako bi mogli prikazat slike

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation ("com.google.android.gms:play-services-auth:20.0.0")
-
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     
 }
